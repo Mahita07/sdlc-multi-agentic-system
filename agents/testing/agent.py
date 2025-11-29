e955f2b8b2022809668e1380be16fa1092187f56
Mahita07 <mahita231102@gmail.com>
2025-11-29 14:39:05 +0530

    Add testing agent

---
+++ agents/testing/agent.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from google.adk.agents import Agent
+
+load_dotenv()
+
+with open("./prompts/testing.txt", "r", encoding="utf-8") as f:
+    testing_instructions = f.read()
+
+
+root_agent = Agent(
+    name="assistant_agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent to write unit tests"
+    ),
+    instruction=(
+        testing_instructions
+    ),
+)
