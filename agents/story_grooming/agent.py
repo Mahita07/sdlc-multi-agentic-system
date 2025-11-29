1a6125e8634fe02c40f2e233de790953e279ed6d
Mahita07 <mahita231102@gmail.com>
2025-11-29 14:38:49 +0530

    Add story groomer agent

---
+++ agents/story_grooming/agent.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from google.adk.agents import Agent
+
+load_dotenv()
+
+with open("./prompts/story-grooming.txt", "r", encoding="utf-8") as f:
+    story_grooming_instructions = f.read()
+
+root_agent = Agent(
+    name="story_groomer_agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent to write user stories"
+    ),
+    instruction=(
+        story_grooming_instructions
+    ),
+)
